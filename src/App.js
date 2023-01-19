0545d2eb8a80301a1ea23938797d769982f9d5a0
Zarina <122347289+ZarinaOrgubaeva@users.noreply.github.com>
2023-01-19 20:08:13 +0600

    style modules

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
+import { useState } from "react";
 function App() {
+  const [usersList, setUsersList] = useState([]);
+  const addUserHandler = (userName, userAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: userName, age: userAge, id: Math.random().toString() },
+      ];
+    });
+  };
+  usersList.sort(function (firstPerson, secondPerson) {
+    if (firstPerson.age > secondPerson.age) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import Card from "../UI/Card";
+import Button from "../UI/Button";
+import classes from "./AddUser.module.css";
+
+const AddUser = ({ onAddUser }) => {
+  const [addUsername, setAddUsername] = useState("");
+  const [addUserAge, setAddUserAge] = useState("");
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    if (addUsername.trim().length > 0) {
+      onAddUser(addUsername, addUserAge);
+      setAddUsername("");
+      setAddUserAge("");
+    } else {
+      alert("fill the form!!!");
+    }
+  };
+  const usernameChangeHandler = (event) => {
+    setAddUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setAddUserAge(event.target.value);
+  };
+  //button disabled
+  let disabledBtn = addUserHandler;
+  return (
+    <div>
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            value={addUsername}
+            onChange={usernameChangeHandler}
+          />
+          <label htmlFor="age">Age (Years)</label>
+          <input
+            id="age"
+            type="number"
+            value={addUserAge}
+            onChange={ageChangeHandler}
+          />
+          <Button type="submit" disabledBtn={!disabledBtn}>
+            Add User
+          </Button>
+        </form>
+      </Card>
+    </div>
+  );
+};
+
+export default AddUser;
